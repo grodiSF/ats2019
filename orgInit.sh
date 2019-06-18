17f31782f84fa9e127b57b008f10e7f065b876b4
Dirk Gronert <dirk.gronert@gmail.com>
2019-06-18 05:02:28 +0200

    just pushing

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,1 @@
-sfdx force:org:create -f config/project-scratch-def.json -s -d 30
-sfdx force:source:push
-sfdx shane:user:password:set -p SF@ATS2o19 -g User -l User --json
-sfdx force:org:display --verbose
-sfdx force:org:open
+sfdx force:source:push -f
