4dcf0cfebba4ff9171f57b3796ddc60a5ef40a78
Dirk Gronert <dirk.gronert@gmail.com>
2019-06-24 13:38:07 +0200

    data ingestion

--- orgInit.sh
+++ orgInit.sh
@@ -4,7 +4,5 @@
 
 #sfdx force:data:soql:query -q "SELECT Id, DeveloperName FROm RecordType WHERE sObjectType = 'Account'" --json | jq -rc '.result.records[] | select(.DeveloperName | IN("ATS19Showroom","ATS19WholesalePartner")) | .DeveloperName + " " + .Id' | while read -r rtname rtid; do sed -i '' "s/$rtname/$rtid/g" ./ats19.json; done;
 
-# cp ./data/ats19*.json .
-# sfdx force:data:tree:import --p ./ats19_plan.json
-
-sfdx force:source:push -f
+cp ./data/ats19*.json .
+sfdx force:data:tree:import --p ./ats19_plan.json
