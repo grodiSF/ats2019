ec0c958e28036f9d8b0d87750a7b8fba2ce7e3a7
Dirk Gronert <dirk.gronert@gmail.com>
2019-06-25 16:29:41 +0200

    modified data extraction

--- orgInit.sh
+++ orgInit.sh
@@ -3,9 +3,11 @@
 
 cp ./data/ats19*.json .
 sfdx force:data:soql:query -q "SELECT Id, DeveloperName FROM RecordType WHERE sObjectType = 'Account'" --json $SFDX_U$ >> ats2019_account_rt_out.json
-cat ./ats2019_account_rt_out.json | jq -rc '.result.records[] | select(.DeveloperName == ("ATS19Showroom","ATS19WholesalePartner")) | .DeveloperName + " " + .Id' | while read -r rtname rtid; do sed -i "s/$rtname/$rtid/g" ./ats19_account_showroom.json; done;
-cat ./ats2019_account_rt_out.json | jq -rc '.result.records[] | select(.DeveloperName == ("ATS19Showroom","ATS19WholesalePartner")) | .DeveloperName + " " + .Id' | while read -r rtname rtid; do sed -i "s/$rtname/$rtid/g" ./ats19_account_wholesalepartner.json; done;
-cat ./ats19_account_showroom.json
-cat ./ats19_account_wholesalepartner.json
-sfdx force:data:tree:import -p ./ats19_plan.json
+#cat ./ats2019_account_rt_out.json | jq -rc '.result.records[] | select(.DeveloperName == ("ATS19Showroom","ATS19WholesalePartner")) | .DeveloperName + " " + .Id' | while read -r rtname rtid; do sed -i "s/$rtname/$rtid/g" ./ats19_account_showroom.json; done;
+#cat ./ats2019_account_rt_out.json | jq -rc '.result.records[] | select(.DeveloperName == ("ATS19Showroom","ATS19WholesalePartner")) | .DeveloperName + " " + .Id' | while read -r rtname rtid; do sed -i "s/$rtname/$rtid/g" ./ats19_account_wholesalepartner.json; done;
+#cat ./ats19_account_showroom.json
+#cat ./ats19_account_wholesalepartner.json
+cat ./ats2019_account_rt_out.json | jq -rc '.result.records[] | select(.DeveloperName == ("ATS19Showroom","ATS19WholesalePartner")) | .DeveloperName + " " + .Id' | while read -r rtname rtid; do sed -i "s/$rtname/$rtid/g" ./ats19_account_wholesalepartner_small.json; done;
+cat ./ats19_account_wholesalepartner_small.json
+sfdx force:data:tree:import -p ./ats19_plan_small.json
 rm ./ats19*.json
