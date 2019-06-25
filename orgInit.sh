274ca48a56da9bf199c0b28103c749b2ed14e7c7
Dirk Gronert <dirk.gronert@gmail.com>
2019-06-25 16:06:26 +0200

    modified data extraction

--- orgInit.sh
+++ orgInit.sh
@@ -4,5 +4,6 @@
 cp ./data/ats19*.json .
 sfdx force:data:soql:query -q "SELECT Id, DeveloperName FROM RecordType WHERE sObjectType = 'Account'" --json $SFDX_U$
 sfdx force:data:soql:query -q "SELECT Id, DeveloperName FROM RecordType WHERE sObjectType = 'Account'" --json $SFDX_U$ | jq -rc '.result.records[] | select(.DeveloperName == ("ATS19Showroom","ATS19WholesalePartner")) | .DeveloperName + " " + .Id' | while read -r rtname rtid; do sed -i "s/$rtname/$rtid/g" ./ats19_account.json; done;
+cat ./ats19_account.json
 sfdx force:data:tree:import -p ./ats19_plan.json
 rm ./ats19*.json
