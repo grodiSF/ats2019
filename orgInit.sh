c4648511840ca2b98625c1d9013b0f31ac4e52f2
Dirk Gronert <dirk.gronert@gmail.com>
2019-06-25 16:12:43 +0200

    modified data extraction

--- orgInit.sh
+++ orgInit.sh
@@ -2,8 +2,9 @@
 # cp ./data/ats19.json .
 
 cp ./data/ats19*.json .
-sfdx force:data:soql:query -q "SELECT Id, DeveloperName FROM RecordType WHERE sObjectType = 'Account'" --json $SFDX_U$
-sfdx force:data:soql:query -q "SELECT Id, DeveloperName FROM RecordType WHERE sObjectType = 'Account'" --json $SFDX_U$ | jq -rc '.result.records[] | select(.DeveloperName == ("ATS19Showroom","ATS19WholesalePartner")) | .DeveloperName + " " + .Id' | while read -r rtname rtid; do sed -i "s/$rtname/$rtid/g" ./ats19_account.json; done;
-cat ./ats19_account.json
+sfdx force:data:soql:query -q "SELECT Id, DeveloperName FROM RecordType WHERE sObjectType = 'Account'" --json $SFDX_U$ >> ats2019_account_rt_out.json
+cat ./ats2019_account_rt_out.json | jq -rc '.result.records[] | select(.DeveloperName == ("ATS19Showroom","ATS19WholesalePartner")) | .DeveloperName + " " + .Id' | while read -r rtname rtid; do sed -i "s/$rtname/$rtid/g" ./ats19_account_showroom.json; done;
+cat ./ats2019_account_rt_out.json | jq -rc '.result.records[] | select(.DeveloperName == ("ATS19Showroom","ATS19WholesalePartner")) | .DeveloperName + " " + .Id' | while read -r rtname rtid; do sed -i "s/$rtname/$rtid/g" ./ats19_account_wholesalepartner.json; done;
+
 sfdx force:data:tree:import -p ./ats19_plan.json
 rm ./ats19*.json
