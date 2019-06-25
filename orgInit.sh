d2fc92ff1886d947577ef7f4aaf901ef178c7687
Dirk Gronert <dirk.gronert@gmail.com>
2019-06-25 16:35:29 +0200

    modified data extraction

--- orgInit.sh
+++ orgInit.sh
@@ -7,6 +7,8 @@ sfdx force:data:soql:query -q "SELECT Id, DeveloperName FROM RecordType WHERE sO
 #cat ./ats2019_account_rt_out.json | jq -rc '.result.records[] | select(.DeveloperName == ("ATS19Showroom","ATS19WholesalePartner")) | .DeveloperName + " " + .Id' | while read -r rtname rtid; do sed -i "s/$rtname/$rtid/g" ./ats19_account_wholesalepartner.json; done;
 #cat ./ats19_account_showroom.json
 #cat ./ats19_account_wholesalepartner.json
+cat ./ats2019_account_rt_out.json | jq -rc '.result.records[] | select(.DeveloperName == ("ATS19Showroom","ATS19WholesalePartner")) | .DeveloperName + " " + .Id' | while read -r rtname rtid; do sed -i "s/$rtname/$rtid/g" ./ats19_account_showroom_small.json; done;
+cat ./ats19_account_showroom_small.json
 cat ./ats2019_account_rt_out.json | jq -rc '.result.records[] | select(.DeveloperName == ("ATS19Showroom","ATS19WholesalePartner")) | .DeveloperName + " " + .Id' | while read -r rtname rtid; do sed -i "s/$rtname/$rtid/g" ./ats19_account_wholesalepartner_small.json; done;
 cat ./ats19_account_wholesalepartner_small.json
 sfdx force:data:tree:import -p ./ats19_plan_small.json
