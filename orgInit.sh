6995b3ffdb0b7baa920c12d45fd377b6bfc8fbd0
Dirk Gronert <dirk.gronert@gmail.com>
2019-06-26 10:24:05 +0200

    safe

--- orgInit.sh
+++ orgInit.sh
@@ -1,6 +1,5 @@
-cp ./data/ats19*.json .
-sfdx force:data:tree:import -p ./ats19_plan.json
-rm ./ats19*.json
-
+#cp ./data/ats19*.json .
+#sfdx force:data:tree:import -p ./ats19_plan.json
+#rm ./ats19*.json
 sfdx force:source:push
 sfdx force:user:permset:assign --permsetname ATS19
